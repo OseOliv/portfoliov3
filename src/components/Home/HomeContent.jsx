09456363b27eb6992cb6a81151d319a830d8699f
Oseas Oliveira <ozzyoliv@gmail.com>
2024-05-19 21:53:38 -0300

    Front page update

--- src/components/Home/HomeContent.jsx
+++ src/components/Home/HomeContent.jsx
@@ -1,30 +1,26 @@
 import React from "react";
 import styles from "./style.module.css";
-import bgHome from '../../assets/img/others/dev.png'
+import bgHome from "../../assets/img/others/dev.png";
 
 const HomeContent = () => {
   return (
     <div className={styles.backgroundContainer}>
       <div className={styles.overlay}></div>
-      <img src={bgHome} alt="background dev" className={styles.backgroundImage} />
+
+      <img
+        src={bgHome}
+        alt="background dev"
+        className={styles.backgroundImage}
+      />
       <div className={styles.container}>
-        <div>
-          <h4 className={styles.welcome}>Olá, meu nome é</h4>
+        <div >
+          <div className={styles.welcomeContainer}>
+            <span className={styles.welcome}>Olá, meu nome é</span>
+          </div>
+            <span className={styles.name}>Oséas Oliveira</span>
         </div>
-        <div>
-          <p className={styles.name}>Oséas Oliveira</p>
-        </div>
-        <div>
-          <p className={styles.textOne}>
-            Há mais de 18 anos, minha fascinação é materializar minha
-            criatividade, <br></br>conhecimento e paixão pela harmonia visual.
-          </p>
-        </div>
-        <div className={styles.textQuote}>
-          <span>
-            <i>“Experience is the name everyone gives to their mistakes.”</i>
-            <br></br>- Oscar Wilde
-          </span>
+        <div className={styles.subtitle}>
+          <span>Frontend Developer.</span>
         </div>
       </div>
     </div>
