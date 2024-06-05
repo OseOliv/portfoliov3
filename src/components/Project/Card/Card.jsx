ab7cccc8034cf7580cf11766db1719e0cf9ecb48
Oseas Oliveira <ozzyoliv@gmail.com>
2024-06-05 20:48:37 -0300

    Update Card.jsx
    update project

--- src/components/Project/Card/Card.jsx
+++ src/components/Project/Card/Card.jsx
@@ -20,13 +20,15 @@ const Card = () => {
           <div className={styles.pageTitle}>
             <span>MEUS PROJETOS</span>
           </div>
+
           <CardItem
-            to="https://github.com/OseOliv/Exemple-Coffee-Shop-WebSite-BootStrap"
-            imgSrc={require("../../../assets/img/project/coffe.png")}
-            title="Coffe Shop"
-            description="Este website de cafeteria integra frameworks e bibliotecas para criar uma experiência online envolvente e funcional. Com uma interface intuitiva, os clientes podem explorar os produtos e serviços oferecidos com facilidade, desfrutando de uma navegação suave e atraente."
-            skills={["HTML", "CSS", "JavaScript", "Bootstrap"]}
+            to="https://github.com/OseOliv/op-memory-game"
+            imgSrc={require("../../../assets/img/project/opmemory.png")}
+            title="OP:Memory Game"
+            description="Este é um jogo da memória envolvendo os adorados personagens da série One Piece, cuidadosamente criado com React, TypeScript e styled-components. Teste sua memória enquanto combina pares de cartas com os personagens favoritos da série."
+            skills={["React.js", "CSS", "TypeScript", "styled-components"]}
           />
+
           <CardItem
             to="https://github.com/OseOliv/pizza-project-app"
             imgSrc={require("../../../assets/img/project/pizzaria.gif")}
@@ -44,30 +46,12 @@ const Card = () => {
           />
 
           <CardItem
-            to="https://github.com/OseOliv/op-memory-game"
-            imgSrc={require("../../../assets/img/project/opmemory.png")}
-            title="OP:Memory Game"
-            description="Este é um jogo da memória envolvendo os adorados personagens da série One Piece, cuidadosamente criado com React, TypeScript e styled-components. Teste sua memória enquanto combina pares de cartas com os personagens favoritos da série."
-            skills={["React.js", "CSS", "TypeScript", "styled-components"]}
-          />
-
-          <CardItem
             to="https://github.com/OseOliv/IMC-APP-TS"
             imgSrc={require("../../../assets/img/project/imc.png")}
             title="IMC App"
             description="Um aplicativo para calcular o Índice de Massa Corpórea (IMC), desenvolvido com TypeScript e CSS Modules. Desfrute de uma ferramenta responsiva e eficiente para monitorar sua saúde e bem-estar."
             skills={["React.js", "TypeScript", "CSS"]}
           />
-
-          <CardItem
-            to="https://github.com/OseOliv/Weather-App-v1"
-            imgSrc={require("../../../assets/img/project/weather.png")}
-            title="Weather App"
-            description="Um modelo de Weather App em React, integrando as APIs do OpenWeatherMap e do GeoDB para fornecer informações precisas sobre o clima. Estilizado com CSS e JavaScript, oferece uma experiência intuitiva e informativa para os usuários."
-            skills={["React.js", "CSS", "JavaScript", "OpenWeather API"]}
-          />
-
-        
 
           {/* <CardItem
             to=""
